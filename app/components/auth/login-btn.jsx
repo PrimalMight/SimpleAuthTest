2b335e1c2774f078bb3159c5c4af0a86ef4969c8
PrimalMight <xbuzov00@vutbr.cz>
2022-12-23 13:44:52 +0100

    second auth try

--- app/components/auth/login-btn.jsx
+++ app/components/auth/login-btn.jsx
@@ -4,6 +4,10 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function LoginButton() {
   const { data: session } = useSession()
+  if(session) {
+      console.log(session.user)
+  }
+
   if (session) {
     return (
       <>
