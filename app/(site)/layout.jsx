f5d9524f71002a223c5a9d1f63bf973d8c0a17a9
PrimalMight <xbuzov00@vutbr.cz>
2022-12-23 13:29:47 +0100

    first auth try

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import LoginButton from '../components/auth/login-btn'
+import Provider from './provider'
 
 export default function RootLayout({ children }) {
     return (
       <html lang='en'>
         <head />
         <body>
+          <Provider>
             <nav>this is navbard idk lol</nav>
+            <LoginButton />
             <main>{children}</main>
+          </Provider>
         </body>
       </html>
     )
