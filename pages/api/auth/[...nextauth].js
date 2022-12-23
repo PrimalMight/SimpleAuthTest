5620353941f180b8854d9fa13033608ed6de078b
PrimalMight <xbuzov00@vutbr.cz>
2022-12-23 16:51:36 +0100

    email provider first try

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,21 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
+
 
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    EmailProvider({
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM
     }),
     // ...add more providers here
   ],
